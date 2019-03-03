1397cfe5441ed2ea4cc93b28b252996571daf6ee
Michael Smith <leogoesgrr@gmail.com>
2019-03-03 09:54:36 -0600

    added photos for 3 board members. 1 needs to be replaced. updated css. added contact form to home page. Started adding addresses.

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,9 +3,15 @@ import Helmet from "react-helmet";
 
 import Layout from '../components/layout';
 
-import pic02 from '../assets/images/pic02.jpg'
+import ReactTooltip from 'react-tooltip';
+import NavigationIcon from '@material-ui/icons/Navigation';
+import Button from '@material-ui/core/Button';
+import EmailPage from './email';
+
+import roger_dickson from '../assets/images/roger_dickson.jpg'
 import pic03 from '../assets/images/pic03.jpg'
-import pic04 from '../assets/images/pic04.jpg'
+import brian_rassavong from '../assets/images/brian_rassavong.jpg'
+import michael_smith from '../assets/images/michael_smith.jpg'
 import ball_and_racket from '../assets/images/ball_and_racket.jpg'
 
 class Homepage extends React.Component {
@@ -42,16 +48,30 @@ class Homepage extends React.Component {
                             <header className="major">
                                 <h2>Schedule</h2>
                             </header>
-                            <p>Thursday from 7:00 PM - 9:00 PM at Academy Park</p>
-                            <p>Friday from 5:30 PM - 8:30 PM at Nolesnville Recreation Center</p>
-                            <p>Sunday from 3:00 PM - 6:00 PM at Brentwood Indoor Sports Complex</p>
+                            <div class="location">
+                                <Button  class="nav" href="https://goo.gl/maps/V6uRMi4NWeK2" data-tip="112 Everbright Ave, Franklin, TN 37064">
+                                        <NavigationIcon></NavigationIcon>
+                                </Button>
+                                <p>Thursday: 7:00 PM - 9:00 PM at Academy Park</p>
+                            </div>
+                            <div class="location">
+                                <Button  class="nav" href="https://goo.gl/maps/enQ7WXaSFXK2" data-tip="112 Everbright Ave, Franklin, TN 37064">
+                                        <NavigationIcon></NavigationIcon>
+                                </Button>
+                                <p>Friday: 5:30 PM - 8:30 PM at Nolesnville Recreation Center</p>
+                            </div>
+                            <div class="location">
+                                <Button  class="nav" href="https://goo.gl/maps/yr8K7kjkjnA2" data-tip="112 Everbright Ave, Franklin, TN 37064">
+                                    <NavigationIcon></NavigationIcon>
+                                </Button>
+                                <p>Sunday: 3:00 PM - 6:00 PM at Brentwood Indoor Sports Complex</p>
+                            </div>
                         </div>
                         <div className="col-6">
                             <header className="major">
                                 <h2>Upcoming Schedule Changes</h2>
                             </header>
-                            <p>Thursday February 21st, 2019 @ Academy Park - <span className="cancelled">Cancelled</span></p>
-                            <p>Friday February 22nd, 2019 @ Nolensville - <span className="cancelled">Cancelled</span></p>
+                            <p>No upcoming schedule changes!</p>
                         </div>
                     </div>
                 </section>
@@ -65,24 +85,24 @@ class Homepage extends React.Component {
                             <p>These are the people responsible for making our club possible.</p>
                         </div>
                         <div className="col-3">
-                            <span className="image fit"><img src={pic04} alt="" /></span>
+                            <span className="image fit admin-image"><img class="admin-image" src={brian_rassavong} alt="" /></span>
                             <h3>President</h3>
                             <p><strong>Brian Rassavong</strong></p>
                         </div>
                         <div className="col-3">
-                            <span className="image fit"><img src={pic02} alt="" /></span>
+                            <span className="image fit admin-image"><img class="admin-image" src={roger_dickson} alt="" /></span>
                             <h3>Vice President</h3>
                             <p><strong>Roger Dickson</strong></p>
                         </div>
                         <div className="col-3">
-                            <span className="image fit"><img src={pic03} alt="" /></span>
-                            <h3>WCPR Coordinator</h3>
-                            <p><strong>Bob Gammon</strong></p>
-                        </div>
-                        <div className="col-3">
-                            <span className="image fit"><img src={pic04} alt="" /></span>
+                            <span className="image fit"><img class="admin-image" src={michael_smith} alt="" /></span>
                             <h3>IT and Secretary</h3>
                             <p><strong>Michael Smith</strong></p>
+                        </div>
+                        <div className="col-3">
+                            <span className="image fit admin-image"><img class="admin-image" src={pic03} alt="" /></span>
+                            <h3>WCPR Coordinator</h3>
+                            <p><strong>Bob Gammon</strong></p>
                         </div>
                     </div>
                 </section>
@@ -98,6 +118,16 @@ class Homepage extends React.Component {
                         </ul>
                     </div>
                 </section>
+
+                <section id="five" class="main style1 special">
+                    <div className="container">
+                        <header className="major">
+                            <h2>Contact Us</h2>
+                        </header>
+                        <EmailPage></EmailPage>
+                    </div>
+                </section>
+                <ReactTooltip />
             </Layout>
         );
     }
