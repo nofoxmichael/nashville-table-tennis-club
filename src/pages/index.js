cdeca13dc37984aa77a0ec0920969ef244c1c68f
Michael Smith <leogoesgrr@gmail.com>
2019-03-09 13:47:29 -0600

    updated google maps links

--- src/pages/index.js
+++ src/pages/index.js
@@ -54,8 +54,8 @@ class Homepage extends React.Component {
                                 <h2>Schedule</h2>
                             </header>
                             <Location url="https://goo.gl/maps/V6uRMi4NWeK2" address="112 Everbright Ave Franklin, TN 37064" time="Thursday: 7:00 PM - 9:00 PM" venue="Academy Park"></Location>
-                            <Location url="https://goo.gl/maps/V6uRMi4NWeK2" address="7250 Nolensville Rd, Nolensville, TN 37135" time="Friday: 5:30 PM - 8:30 PM" venue="Nolensville Rec Center"></Location>
-                            <Location url="https://goo.gl/maps/V6uRMi4NWeK2" address="920 Heritage Way, Brentwood, TN 37027" time="Sunday: 3:00 PM - 6:00 PM" venue="Indoor Sports Complex"></Location>
+                            <Location url="https://goo.gl/maps/3LuJE47J1ty" address="7250 Nolensville Rd, Nolensville, TN 37135" time="Friday: 5:30 PM - 8:30 PM" venue="Nolensville Rec Center"></Location>
+                            <Location url="https://goo.gl/maps/9gQqLFvfpxD2" address="920 Heritage Way, Brentwood, TN 37027" time="Sunday: 3:00 PM - 6:00 PM" venue="Indoor Sports Complex"></Location>
                         </div>
                         <div className="col-6">
                             <header className="major">
