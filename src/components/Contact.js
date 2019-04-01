fd360612ff93e79fcd43855e23c405621772c036
Michael Smith <leogoesgrr@gmail.com>
2019-03-31 21:28:50 -0500

    updated css to lessen console errors and be more react-ive. Changed css on bonner and images to be more responsive. Made schedule section rendor more responsively using @media query. Using dummy image for Brian right now until we can all get photos done that are the same aspect ratio and uniform in composition.

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,10 +3,10 @@ import React from "react";
 class Contact extends React.Component {
     render() {
         return (
-            <form class="fieldset" action="https://formspree.io/nashvillettc@gmail.com" method="POST">
-                <input class="email-form" type="text" name="name" placeholder="name" />
-                <input class="email-form" type="email" name="_replyto" placeholder="email" />
-                <textarea class="email-text" name="message" placeholder="message"></textarea>
+            <form className="fieldset" action="https://formspree.io/nashvillettc@gmail.com" method="POST">
+                <input className="email-form" type="text" name="name" placeholder="name" />
+                <input className="email-form" type="email" name="_replyto" placeholder="email" />
+                <textarea className="email-text" name="message" placeholder="message"></textarea>
                 <input type="submit" value="Send" />
             </form>
         )
