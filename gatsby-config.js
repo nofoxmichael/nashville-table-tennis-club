faeffe2bf62a862cff48167e32ceb8c8d1ad8dae
therealphatmike <leogoesgrr@gmail.com>
2019-04-02 07:34:28 -0500

    Updated config that generates manifest.
    Color theming for mobile, now more coherent.

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,8 +12,8 @@ module.exports = {
         name: 'Nashville Table Tennis Club',
         short_name: 'NTTC',
         start_url: '/',
-        background_color: '#663399',
-        theme_color: '#663399',
+        background_color: '#FFFFFF',
+        theme_color: '#FFFFFF',
         display: 'minimal-ui',
         icon: 'src/assets/images/banner_small.png', // This path is relative to the root of the site.
       },
