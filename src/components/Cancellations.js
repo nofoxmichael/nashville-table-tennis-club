bb9315b4e057c299e53675cb6cd674a696301a34
Michael Smith <leogoesgrr@gmail.com>
2019-06-05 18:47:47 -0500

    started setting up cms graphql

--- src/components/Cancellations.js
+++ src/components/Cancellations.js
@@ -1,7 +1,9 @@
 import React from 'react';
 
 class Cancellations extends React.Component {
-
+    render() {
+        <p>{this.props.cancellations}</p>
+    }
 }
 
 export default Cancellations;
--- src/components/UpcomingScheduleChanges.js
+++ src/components/UpcomingScheduleChanges.js
@@ -1,16 +1,21 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { StaticQuery, graphql } from 'gatsby';
 
 import Cancellations from './Cancellations';
 
 class UpcomingScheduleChanges extends React.Component {
     render() {
+        const { data } = this.props;
+        const { edges: cancellations } = data.allMarkdownRemark;
+
         return (
             <div className="col-6">
                 <header className="major">
                     <h2>Upcoming Schedule Changes</h2>
                 </header>
                 {this.props.cancellations ? (
-                    <Cancellations></Cancellations>
+                    <Cancellations cancellations={cancellations}></Cancellations>
                 ) : (
                     <p>No upcoming cancellations!</p>
                 )}
@@ -19,4 +24,26 @@ class UpcomingScheduleChanges extends React.Component {
     }
 }
 
-export default UpcomingScheduleChanges;
+
+UpcomingScheduleChanges.propTypes = {
+    data: PropTypes.shape({
+        allMarkdownRemark: PropTypes.shape({
+            edges: PropTypes.array
+        })
+    })
+}
+
+export default () => (
+    <StaticQuery
+        query={graphql`
+            {
+                cancellations {
+                    date
+                    location
+                    type
+                }
+            }
+        `}
+        render={(data) => <UpcomingScheduleChanges cancellations={data} />}
+    />
+);
