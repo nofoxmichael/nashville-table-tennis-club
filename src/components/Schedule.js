470dee17206b87243d1c2842da85a8329cd0d077
Michael Smith <leogoesgrr@gmail.com>
2019-06-05 18:50:21 -0500

    updated Thursday meet time and updated upcoming schedule to show june 9th cancellation

--- src/components/Schedule.js
+++ src/components/Schedule.js
@@ -12,7 +12,7 @@ class Schedule extends React.Component {
                         <header className="major">
                             <h2>Schedule</h2>
                         </header>
-                        <Location url="https://goo.gl/maps/V6uRMi4NWeK2" address="112 Everbright Ave Franklin, TN 37064" time="Thursday: 7:00 PM - 9:00 PM" venue="Academy Park"></Location>
+                        <Location url="https://goo.gl/maps/V6uRMi4NWeK2" address="112 Everbright Ave Franklin, TN 37064" time="Thursday: 6:00 PM - 9:00 PM" venue="Academy Park"></Location>
                         <Location url="https://goo.gl/maps/3LuJE47J1ty" address="7250 Nolensville Rd, Nolensville, TN 37135" time="Friday: 5:30 PM - 8:30 PM" venue="Nolensville Rec Center"></Location>
                         <Location url="https://goo.gl/maps/9gQqLFvfpxD2" address="920 Heritage Way, Brentwood, TN 37027" time="Sunday: 3:00 PM - 6:00 PM" venue="Indoor Sports Complex"></Location>
                     </div>
--- src/components/UpcomingScheduleChanges.js
+++ src/components/UpcomingScheduleChanges.js
@@ -12,7 +12,7 @@ class UpcomingScheduleChanges extends React.Component {
                 {this.props.cancellations ? (
                     <Cancellations></Cancellations>
                 ) : (
-                    <p>No upcoming cancellations!</p>
+                    <p>Sunday June, 9th at Indoor Sports Complex is <span className="cancelled">CANCELLED!</span></p>
                 )}
             </div>
         )
