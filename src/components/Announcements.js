d39757ec94c39b675e66cfbf98d1ed35037592c2
T Michael Smith <timothy.smith2@asurion.com>
2020-02-07 15:57:23 -0800

    first draft of announcements component

---
+++ src/components/Announcements.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+class Anncouncements extends React.Component {
+
+    render() {
+        return (
+            <section id="two" className="main style2 special">
+                <div className="grid-wrapper">
+                    <div className="col-12">
+                        <header className="major">
+                            <h2>Anncouncements</h2>
+                        </header>
+                    </div>
+                </div>
+                <p> Tenessee Senior Olympics Table Tennis State Finals: June 23, 2020</p>
+                <ul className="actions uniform">
+                    <li><a href="http://www.tnseniorolympics.com/table-tennis" target="_blank" rel="noopener noreferrer" className="button special">More Info</a></li>
+                </ul>
+
+                <p> Some other random announcements that have a more info button and a lot of text</p>
+                <ul className="actions uniform">
+                    <li><a href="http://www.tnseniorolympics.com/table-tennis" target="_blank" rel="noopener noreferrer" className="button special">More Info</a></li>
+                </ul>
+            </section>
+        )
+    }
+}
+
+export default Anncouncements
